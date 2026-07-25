b63b184c240ce8a6c609dafb5cbf40852e89ebb2
Rodrigo Cunha <rodrigoc.frontend@gmail.com>
2026-07-25 14:35:02 -0300

    Initial Commit

---
+++ src/app/app.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, signal } from '@angular/core';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.scss',
+})
+export class App implements OnInit {
+  protected readonly title = signal('giulia-s-pergentino');
+
+  ngOnInit(): void {
+    gsap.registerPlugin(ScrollTrigger);
+
+    // 1. Animação principal controlada 100% pelo scroll do usuário
+    gsap.fromTo(
+      '.video',
+      { y: '100vh' },
+      {
+        y: '0vh',
+        ease: 'none', // Deve ser 'none' para o movimento do scroll ser 1:1 e linear
+        scrollTrigger: {
+          trigger: '.video',
+          start: 'top bottom',
+          end: 'top top',
+          snap: {
+            // Regra dos 50%: se progress < 0.5 volta pra 0, se >= 0.5 vai pra 1
+            snapTo: (value) => (value < 0.4 ? 0 : 1),
+            delay: 0.1, // Espera 0.1s após soltar o scroll para ativar
+            duration: { min: 6, max: 6 },
+            ease: 'elastic.out(1, 0.5)', // O efeito de mola ao travar na posição
+          },
+          scrub: 1, // Mantém o scroll fluido e no controle do usuário
+
+          // Quando o usuário para de rolar a página
+        },
+      },
+    );
+  }
+}
