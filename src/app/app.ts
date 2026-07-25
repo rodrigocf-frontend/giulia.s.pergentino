d52cf6622b1a1aa6827c659be0e7168dfec80a88
Rodrigo Cunha <rodrigoc.frontend@gmail.com>
2026-07-25 14:57:11 -0300

    feat: first animations

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, signal } from '@angular/core';
 import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/all';
+import { ScrollTrigger, SplitText } from 'gsap/all';
 
 @Component({
   selector: 'app-root',
@@ -29,7 +29,7 @@ export class App implements OnInit {
             // Regra dos 50%: se progress < 0.5 volta pra 0, se >= 0.5 vai pra 1
             snapTo: (value) => (value < 0.4 ? 0 : 1),
             delay: 0.1, // Espera 0.1s após soltar o scroll para ativar
-            duration: { min: 6, max: 6 },
+            duration: { min: 0.4, max: 5 },
             ease: 'elastic.out(1, 0.5)', // O efeito de mola ao travar na posição
           },
           scrub: 1, // Mantém o scroll fluido e no controle do usuário
@@ -38,5 +38,16 @@ export class App implements OnInit {
         },
       },
     );
+
+    gsap.registerPlugin(SplitText);
+    let split = SplitText.create('.title', { type: 'chars' });
+
+    // now animate the characters in a staggered fashion
+    gsap.from(split.chars, {
+      duration: 0.5,
+      x: 50, // animate from 100px below
+      autoAlpha: 0, // fade in from opacity: 0 and visibility: hidden
+      stagger: 0.05, // 0.05 seconds between each
+    });
   }
 }
